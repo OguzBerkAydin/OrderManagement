fd15eec99885ae038bf1ba4c3ea042aa72a84768
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2024-02-05 20:49:42 +0300

    ...

---
+++ BusinessLayer/Concrete/TestimonialManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class TestimonialManager : ITestimonialService
+    {
+        private readonly ITestimonialDal _testimonialDal;
+
+        public TestimonialManager(ITestimonialDal testimonialDal)
+        {
+            _testimonialDal = testimonialDal;
+        }
+
+        public void TAdd(Testimonial entity)
+        {
+            _testimonialDal.Add(entity);
+        }
+
+        public void TDelete(Testimonial entity)
+        {
+            _testimonialDal.Delete(entity);
+        }
+
+        public Testimonial TGet(int id)
+        {
+            return _testimonialDal.Get(id);
+        }
+
+        public List<Testimonial> TGetAll()
+        {
+            return _testimonialDal.GetAll();
+        }
+
+        public void TUpdate(Testimonial entity)
+        {
+            _testimonialDal.Update(entity);
+        }
+    }
+}
