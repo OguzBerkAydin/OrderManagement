4196b6c96c62668e91bdc2df99d39b4ed7640dcb
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2024-02-13 17:37:37 +0300

    cors added

--- SignalRWebUI/Program.cs
+++ SignalRWebUI/Program.cs
@@ -1,37 +1,52 @@
 namespace SignalRWebUI
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddHttpClient();
-            builder.Services.AddControllersWithViews();
+			builder.Services.AddCors(opt =>
+			{
+				opt.AddPolicy("CorsPolicy", builder =>
+				{
+					builder.AllowAnyHeader()
+					.AllowAnyMethod()
+					.SetIsOriginAllowed((host) => true)
+					.AllowCredentials();
 
-            var app = builder.Build();
+				});
+			});
+			builder.Services.AddSignalR();
+			// Add services to the container.
+			builder.Services.AddHttpClient();
+			builder.Services.AddControllersWithViews();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+			var app = builder.Build();
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
-            app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseCors("CorsPolicy");
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
-            app.Run();
-        }
-    }
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+
+			app.Run();
+		}
+	}
 }
