0f7470828fcaa6df14a3d2225852f983b964871f
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2024-02-26 17:24:53 +0300

    2 statistic called at the same time

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -1,15 +1,29 @@
-﻿using DataAccessLayer.Concrete;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using Microsoft.AspNetCore.SignalR;
 
 namespace SignalRApi.Hubs
 {
 	public class SignalRHub : Hub
 	{
-		SignalRContext context = new SignalRContext();
+		private readonly ICategoryService _categoryService;
+		private readonly IProductService _productService;
+
+		public SignalRHub(ICategoryService categoryService, IProductService productService)
+		{
+			_categoryService = categoryService;
+			_productService = productService;
+		}
+
 		public async Task SendCategoryCount()
 		{
-			var value = context.Categories.Count();
+			var value = _categoryService.TCategoryCount();
 			await Clients.All.SendAsync("ReceiveCategoryCount", value);
+		}
+		public async Task SendProductCount()
+		{
+			var value = _productService.TProductCount();
+			await Clients.All.SendAsync("ReceiveProductCount", value);
 		}
 	}
 }
