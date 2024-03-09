08df1a58c0409b697ff2a7b8d73d5ac8d753ff75
Oguz Berk AYDIN <ouzberkaydin@gmail.com>
2024-03-09 03:50:41 +0300

    Update Contact

--- DataAccessLayer/Migrations/SignalRContextModelSnapshot.cs
+++ DataAccessLayer/Migrations/SignalRContextModelSnapshot.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
-    [DbContext(typeof(SignalRContext))]
+    [DbContext(typeof(SignalRContext))] 
     partial class SignalRContextModelSnapshot : ModelSnapshot
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
