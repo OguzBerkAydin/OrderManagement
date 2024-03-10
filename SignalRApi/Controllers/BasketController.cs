8cfe24c599e213629d60a057e0195138e58d5de7
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2024-03-10 22:48:59 +0300

    Controller added

---
+++ SignalRApi/Controllers/BasketController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketService _basketService;
+
+        public BasketController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+        [HttpGet]
+        public IActionResult GetBasketByRestaurantTableID(int id)
+        {var values = _basketService.TGetBasketByRestaurantTableNumber(id);
+            return Ok(values);
+        }
+    }
+}
