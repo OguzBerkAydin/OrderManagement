46ff701c66ae15c243cef32861d551aba2ea6337
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2024-03-11 17:19:39 +0300

    BasketListByRestaurantTableWithProductName method added temporarly

--- SignalRApi/Controllers/BasketController.cs
+++ SignalRApi/Controllers/BasketController.cs
@@ -1,6 +1,9 @@
 ﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SignalRApi.Models;
 
 namespace SignalRApi.Controllers
 {
@@ -14,10 +17,29 @@ namespace SignalRApi.Controllers
         {
             _basketService = basketService;
         }
+
         [HttpGet]
         public IActionResult GetBasketByRestaurantTableID(int id)
         {var values = _basketService.TGetBasketByRestaurantTableNumber(id);
             return Ok(values);
         }
+
+        [HttpGet("BasketListByRestaurantTableWithProductName")]
+        public IActionResult BasketListByRestaurantTableWithProductName(int id)
+        {
+            using var context = new SignalRContext();
+            var values = context.Baskets.Include(x => x.Product).Where(y => y.RestaurantTableId == id).Select(z => new RsultBasketListWithProducts()
+            {
+                BasketID = z.BasketID,
+                Count = z.Count,
+                Price = z.Price,
+                ProductId = z.ProductId,
+                ProductName = z.Product.ProductName,
+                RestaurantTableId = id,
+                TotalPrice = z.TotalPrice
+            }).ToList();
+            return Ok(values);
+        }
+
     }
 }
---
+++ SignalRApi/Models/RsultBasketListWithProducts.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalRApi.Models
+{
+    public class RsultBasketListWithProducts
+    {
+        public int BasketID { get; set; }
+        public decimal Price { get; set; }
+        public decimal Count { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int ProductId { get; set; }
+        public int RestaurantTableId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
