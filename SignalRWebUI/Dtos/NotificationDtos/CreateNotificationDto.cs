8688018b1500617d531e46cd7e42803c4c97f632
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2024-04-27 13:16:57 +0300

    notifications crud added for webui

--- SignalRWebUI/Dtos/NotificationDtos/CreateNotificationDto.cs
+++ SignalRWebUI/Dtos/NotificationDtos/CreateNotificationDto.cs
@@ -5,7 +5,6 @@
 		public string Description { get; set; }
 		public string Type { get; set; }
 		public string Icon { get; set; }
-		public DateTime Date { get; set; }
 		public bool Status { get; set; }
 	}
 }
