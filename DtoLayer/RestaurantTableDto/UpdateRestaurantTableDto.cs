85a86c5ffaac8a843dece7efebc601c17064a7fb
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2024-05-15 10:33:47 +0300

    crud added

---
+++ DtoLayer/RestaurantTableDto/UpdateRestaurantTableDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DtoLayer.RestaurantTableDto
+{
+	public class UpdateRestaurantTableDto
+	{
+		public int RestaurantTableId { get; set; }
+		public string Name { get; set; }
+		public bool Status { get; set; }
+	}
+}
--- SignalRApi/Controllers/RestaurantTableController.cs
+++ SignalRApi/Controllers/RestaurantTableController.cs
@@ -1,4 +1,6 @@
 ﻿using BusinessLayer.Abstract;
+using DtoLayer.RestaurantTableDto;
+using EntityLayer.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +21,49 @@ namespace SignalRApi.Controllers
 		public IActionResult TotalTableCount()
 		{
 			return Ok(_restaurantTableService.TTotalTableCount());
+		}
+
+		[HttpGet]
+		public IActionResult RestaurantTableList()
+		{
+			var values = _restaurantTableService.TGetAll();
+			return Ok(values);
+		}
+		[HttpPost]
+		public IActionResult CreateRestaurantTable(CreateRestaurantTableDto createRestaurantTableDto)
+		{
+			RestaurantTable restaurantTable = new RestaurantTable()
+			{
+				Name = createRestaurantTableDto.Name,
+				Status = false
+			};
+			_restaurantTableService.TAdd(restaurantTable);
+			return Ok("Masa eklendi.");
+		}
+		[HttpDelete("{id}")]
+		public IActionResult DeleteRestaurantTable(int id)
+		{
+			var value = _restaurantTableService.TGet(id);
+			_restaurantTableService.TDelete(value);
+			return Ok("Masa Silindi.");
+		}
+		[HttpPut]
+		public IActionResult UpdateRestaurantTable(UpdateRestaurantTableDto updateRestaurantTableDto)
+		{
+			RestaurantTable restaurantTable = new RestaurantTable()
+			{
+				RestaurantTableId = updateRestaurantTableDto.RestaurantTableId,
+				Name = updateRestaurantTableDto.Name,
+				Status = false
+			};
+			_restaurantTableService.TUpdate(restaurantTable);
+			return Ok("Masa Güncellendi.");
+		}
+		[HttpGet("{id}")]
+		public IActionResult GetRestaurantTable(int id)
+		{
+			var value = _restaurantTableService.TGet(id);
+			return Ok(value);
 		}
 	}
 }
